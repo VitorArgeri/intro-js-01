c243bdfd0f6f077395f214272d6170c3338fe8d8
vitor.argeri@aluno.senai.sp <vitor.argeri@aluno.senai.sp>
2025-02-04 09:43:15 -0300

    create: main math functions

--- assets/scripts/math.script.js
+++ assets/scripts/math.script.js
@@ -1,5 +1,24 @@
-function calcular() {
+function somar() {
     let numA = Number(document.getElementById("numA").value);
     let numB = Number(document.getElementById("numB").value);
     document.getElementById('result').innerHTML = numA + numB;
 }
+
+function subtrair() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    document.getElementById('result').innerHTML = numA - numB;
+}
+
+function multiplicar() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    document.getElementById('result').innerHTML = numA * numB;
+}
+
+function dividir() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    document.getElementById('result').innerHTML = numA / numB;
+}
+
