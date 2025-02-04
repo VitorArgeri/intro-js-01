67750041e09a4b079d3c776817b96d7db536cce2
vitor.argeri@aluno.senai.sp <vitor.argeri@aluno.senai.sp>
2025-02-04 09:33:39 -0300

    feat: create calculate function

---
+++ assets/scripts/math.script.js
@@ -0,0 +1,5 @@
+function calcular() {
+    let numA = Number(document.getElementById("numA").value);
+    let numB = Number(document.getElementById("numB").value);
+    document.getElementById('result').innerHTML = numA + numB;
+}
